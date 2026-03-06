cf7ce38ab9dc07578267356a4f2182b45a926780
dgTr3 <Edugta31@gmail.com>
2026-03-05 20:45:16 -0600

    completo

--- Do-While14/Program.cs
+++ Do-While14/Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Determinar número mayor hasta ingresar 0");
+int numero;
+int mayor= int.MinValue;
+do
+{
+    Console.Write("Ingrese un número (0 para terminar): ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > mayor)
+    {
+        mayor = numero;
+    }
+} while (numero != 0);
+Console.WriteLine("Número mayor:" +mayor);
